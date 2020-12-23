32695fd8f1d3f3c39d4b1fe7fb1a2486d599ad9a
Monnier <mathieu.monnier@useweb.com>
2020-12-23 15:39:12 +0100

    Purge tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  // purge: [
-  //   './components/**/*.{vue,js}',
-  //   './layouts/**/*.vue',
-  //   './pages/**/*.vue',
-  //   './plugins/**/*.{js,ts}',
-  //   './nuxt.config.{js,ts}',
-  // ],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   darkMode: false,
   theme: {
     container: {
